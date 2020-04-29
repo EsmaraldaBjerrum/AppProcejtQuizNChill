ae2267dc0e785f9489f6553e70d59f8fefd97f96
Esmaralda Dorthea Bjerrum <au593888@uni.au.dk>
2020-04-29 21:43:14 +0200

    Database og lidt sager

--- app/src/main/java/dk/appproject/quiznchill/MainActivity.java
+++ app/src/main/java/dk/appproject/quiznchill/MainActivity.java
@@ -11,13 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -29,6 +22,8 @@ import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -36,15 +31,17 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import org.json.JSONObject;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -96,10 +93,72 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                sendGraphRequest(user);
+
+                //Test for menu
+                //startActivity(new Intent(MainActivity.this, MenuActivity.class));
+
+                //Test for question
+                Intent goToQuestion = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(goToQuestion);
+
+                //FirebaseUser user = firebaseAuth.getCurrentUser();
+                //sendGraphRequest(user);
             }
         });
+
+
+        //Firebase database ADDING
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Question Q1 = new Question("HVem er en hat?", "Din mor", "Bente B", "Dronning M");
+        Question Q2 = new Question("HVem er en kat?", "Hunden", "Katten", "Klatten");
+
+        Question [] goodQ = {
+                Q1,
+                Q2
+        };
+
+        // Create a new user with a first and last name
+        Map<String, Object> personaleQuiz = new HashMap<>();
+        personaleQuiz.put("name", "De gode spørgsmål");
+        personaleQuiz.put("questions", Arrays.asList(goodQ));
+        db.collection("PersonaleQuizzes").document("De gode spørgsmål").set(personaleQuiz);
+
+        // Add a new document with a generated ID
+        db.collection("PersonaleQuizzes")
+                .add(personaleQuiz)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
+        //FIREBASE GETTING
+        DocumentReference docRef = db.collection("PersonaleQuizzes").document("De gode spørgsmål");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
 
     //Kald for venner
